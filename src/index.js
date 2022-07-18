07eb62b2b39be6391722fd86555016e3f36d279b
Paul Jung <pbnjcub@gmail.com>
2022-07-18 17:32:45 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let newTask = document.getElementById('new-task-description')
+    addTaskToList(newTask)
+    form.reset()
+
+  })
 });
+
+function addTaskToList(todo){
+  let li = document.createElement('li')
+  li.setAttribute('id','ad-task')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = ' x '
+  li.textContent = `${todo.value}  `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+  
+
+  // let taskListItem = document.getElementById('tasks')
+  // taskListItem.textContent = todo.value
+}
+
+function deleteTask(e){
+  let btnRemove = document.getElementById('ad-task')
+  btnRemove.remove()
+}
+
